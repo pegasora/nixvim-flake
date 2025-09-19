a4d90875fb073edb6aa5ffda3ff3530b97f84b24
pegasora <dawsonhburgess@gmail.com>
2025-09-18 17:45:28 -0700

    conform!
    still need to figure out why ti cant work with some, do i need to
    download more things?

---
+++ config/plugins/conform.nix
@@ -0,0 +1,38 @@
+{
+  plugins.conform-nvim = {
+    enable = true;
+    settings = {
+      default_format_opts = {
+        timeout_ms = 3000;
+        async = false;
+        quiet = false;
+        lsp_format = "fallback";
+      };
+      format_on_save = { 
+        timeout_ms = 500;
+        lsp_format = "fallback";
+      };
+      formatters_by_ft = {
+        lua = [ "stylua" ];
+        python = [ "ruff" ];
+        sh = [ "shfmt" ];
+        rust = [ "rustfmt" ];
+        nix = [ "nixfmt" ];
+
+        javascript = [ "prettier" ];
+        typescript = [ "prettier" ];
+        css = [ "prettier" ];
+        html = [ "prettier" ];
+        json = [ "prettier" ];
+        yaml = [ "prettier" ];
+      };
+      formatters = {
+        injected = { 
+          options = {
+            ignore_errors = true;
+          };
+        };
+      };
+    };
+  };
+}
