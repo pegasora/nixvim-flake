fa246c39047ffee74f9b8f3bf963dfd4d682e83b
pegasora <dawsonhburgess@gmail.com>
2025-09-18 17:45:00 -0700

    set first theme
    catppuccin ofc
    
    may add some more later or change color rules

--- config/plugins/colorscheme.nix
+++ config/plugins/colorscheme.nix
@@ -1,3 +1,25 @@
 {
-  colorschemes.catppuccin.enable = true;
+  colorschemes = { 
+    catppuccin = { 
+      enable = true;
+      settings = {
+        flavour = "mocha";
+        integrations = {
+          treesitter = true;
+          nvimtree = true;
+        };
+        styles = {
+          booleans = [
+            "bold"
+            "italic"
+          ];
+          conditionals = [
+            "bold"
+            "italic"
+          ];
+        };
+      term_colors = true;
+      };
+    };
+  };
 }
