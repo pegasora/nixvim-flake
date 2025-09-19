6087fa57c6babb744b390a80d11efb3a7caa6f44
pegasora <dawsonhburgess@gmail.com>
2025-09-18 17:44:43 -0700

    bufferline done

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -2,6 +2,20 @@
   plugins = {
     bufferline = {
       enable = true;
+      settings = { 
+        options = {
+          numbers = "none";
+          close_command = "bdelete! %d";
+          right_mouse_command = "bdelete! %d";
+          left_mouse_command = "buffer %d";
+          middle_mouse_command = "bdelete! %d";
+          indicator = {
+            icon = "▎";
+            style = "icon";
+          };
+          mode = "tabs";
+        };
+      };
     };
     web-devicons.enable = true;
   };
